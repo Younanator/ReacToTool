76b67d9bac4427a5c6add5fbe42a2a65e388a433
ranqed300 <cyounan96@gmail.com>
2020-02-07 12:04:13 +1100

    Make it move from tray

--- public/electron.js
+++ public/electron.js
@@ -1,13 +1,41 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-
+const Menu = electron.Menu
+const Tray = electron.Tray
 const path = require('path');
 const isDev = require('electron-is-dev');
 
 let mainWindow;
+let isQuiting;
+let tray;
 
 function createWindow() {
+  app.on('before-quit', function () {
+    isQuiting = true;
+  });
+
+  tray = new Tray(path.join(__dirname, 'download.png'));
+
+  tray.setContextMenu(Menu.buildFromTemplate([
+    {
+      label: 'Show App', click: function () {
+        mainWindow.show();
+      }
+    },
+    {
+      label: 'Quit', click: function () {
+        isQuiting = true;
+        app.quit();
+      }
+    },
+    
+  ]));
+
+  tray.on('double-click', () => {
+    mainWindow.show();
+  })
+
   mainWindow = new BrowserWindow({
     width: 500,
     height: 420,
@@ -15,14 +43,26 @@ function createWindow() {
       devTools: false
       } */
   })
+  
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   if (isDev) {
     // Open the DevTools.
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
    // mainWindow.webContents.openDevTools();
   }
+  mainWindow.on('minimize',function(event){
+    event.preventDefault();
+    mainWindow.hide();
+});
    mainWindow.show()
-  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.on('closed', () => {
+    if (!isQuiting) {
+      event.preventDefault();
+      window.hide();
+      event.returnValue = false;
+    }
+  });
+
 }
 
 app.on('ready', createWindow);
